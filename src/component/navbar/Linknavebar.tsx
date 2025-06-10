a8679402977cbbc3063a18bebb4c495e0bd4b427
Wayykoe <117815509+Wayykoe@users.noreply.github.com>
2025-06-10 16:16:11 +0200

    main

--- src/component/navbar/Linknavebar.tsx
+++ src/component/navbar/Linknavebar.tsx
@@ -9,7 +9,7 @@ export default function LinkNavbar() {
         <div className="hidden md:flex space-x-6 text-ghostly-white text-sm sm:text-base">
             {navItems.map((item) => (
                 <a key={item.id} href={`#${item.id}`}>
-                    <p className="text-white hover:text-white transition duration-300">
+                    <p className="text-white hover:text-gray-400 transition duration-100">
                         {item.label}
                     </p>
                 </a>
