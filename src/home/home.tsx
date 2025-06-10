0847d00594194ce273d873337497c460b3e7d273
Wayykoe <117815509+Wayykoe@users.noreply.github.com>
2025-06-10 15:08:22 +0200

    main

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -2,19 +2,21 @@ import Navbar from '../component/navbar/navbar'
 import Hero from './hero/hero'
 import Sessions from './sessions/sessions'
 import About from './about/about'
-import Contact from './contact/contact'
-import Footer from '../component/footer/footer'
-import Navbar from "../component/navbar/navbar.tsx";
+// import Contact from './contact/contact'
+// import Footer from '../component/footer/footer'
+
 
 export default function Home() {
   return (
     <>
+        <div className="w-screen">
         <Navbar />
         <Hero />
         <Sessions />
         <About />
-        <Contact />
-        <Footer />
+        {/*<Contact />*/}
+        {/*<Footer />*/}
+        </div>
     </>
   )
 }
