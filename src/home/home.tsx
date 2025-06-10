3d240347db98032a37726c50b923f6f6217dbfbd
Wayykoe <117815509+Wayykoe@users.noreply.github.com>
2025-06-10 14:57:39 +0200

    main

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,9 +1,13 @@
 //import React from 'react'
 
+import Navbar from "../component/navbar/navbar.tsx";
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
+    return (<div className="text-ghostly-white font-inter overflow-x-hidden">
+            <Navbar></Navbar>
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        </div>
+    )
 }
