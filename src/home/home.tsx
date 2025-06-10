d7463d08af75c8e69ee8233dea6428ed316645d1
AITBOUKHA Elias <elias.aitboukha@gmail.com>
2025-06-10 14:58:55 +0200

    base archi

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -9,27 +9,14 @@ import Navbar from "../component/navbar/navbar.tsx";
 
 export default function Home() {
   return (
-    <html lang="fr">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>La Maison Horrifique - Escape Games d'Horreur</title>
-        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Creepster&family=Nosifer&family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="bg-black text-ghostly-white font-inter overflow-x-hidden">
+    <>
         <Navbar />
         <Hero />
         <Sessions />
         <About />
         <Contact />
         <Footer />
-
-      </body>
-    </html>
+    </>
   )
 }
 
