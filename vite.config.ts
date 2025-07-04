b264bf72da739ef4932f0518df7bfad41242f87d
AITBOUKHA Elias <elias.aitboukha@gmail.com>
2025-07-04 16:57:11 +0200

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,15 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { mode } from './src/lib/utils'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: mode === 'production' ? '/lamaisonhorifique/' : '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/La-Maison-Horrifique/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}))
 
